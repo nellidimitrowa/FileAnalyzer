e0d3f0bfe6c4c72648077215e8d2154d68cab0ed
nellidimitrowa <nellidimitrowa@gmail.com>
2021-06-10 00:10:40 +0300

    Created new App frame

--- src/main/java/com/analyze/fileanalyzer/App.java
+++ src/main/java/com/analyze/fileanalyzer/App.java
@@ -1,13 +1,4 @@
 package com.analyze.fileanalyzer;
 
-import org.apache.tika.Tika;
-
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-
 public class App {
-    public static void main(String[] args) throws IOException {
-        Main mainWindow = new Main();
-        mainWindow.createMainWindow();
-    }}
+}
